a75848d84babcc1259a3e0ffcc1823e3e0fd9011
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2021-12-06 16:47:43 +0100

    Initial project

---
+++ src/test/java/se/baselcode/recipedatabaseassignment/RecipeDatabaseAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.baselcode.recipedatabaseassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeDatabaseAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
